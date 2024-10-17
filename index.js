9d4455c42ce857e4594a0687aa6c7c770bea9a05
ncmsilva <chathurasilva98@gmail.com>
2024-10-17 16:04:04 +0530

    mongo db connection tested and .env added

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 
+dotenv.config();
 const app = express()
 
+const conn_str = process.env.mongo_con_str;
+console.log(conn_str)
 
+mongoose.connect(conn_str).then(
+    ()=>{
+        console.log("Connected to the database")
+    }
+).catch(
+    (err)=>{
+        console.log(`Connection fail message ${err}`)
+    }
+)
 
 app.listen(5002,(req,res)=>
 {
